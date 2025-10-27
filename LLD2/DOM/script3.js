739b20bcc6de0a08faf0add26175589025e26a42
Ayush <ayush.raj.sd@gmail.com>
2025-10-27 12:26:03 +0530

    lecture 8 - DOM events

---
+++ LLD2/DOM/script3.js
@@ -0,0 +1,18 @@
+const removeAllBtn = document.querySelector("#removeAll");
+const ul = document.querySelector("ul");
+const allDeleteButtons = document.querySelectorAll(".delete_button");
+
+for (let i = 0; i < allDeleteButtons.length; i++) {
+  allDeleteButtons[i].addEventListener("click", function (e) {
+    // go to the target element and then delete the parent
+    e.target.parentElement.remove();
+  });
+}
+
+removeAllBtn.addEventListener("click", function () {
+  //   const allListItems = ul.querySelectorAll("li");
+  //   for (let i = 0; i < allListItems.length; i++) {
+  //     ul.removeChild(allListItems[i]);
+  //   }
+  ul.innerHTML = "";
+});
