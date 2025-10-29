fbc16fdb61cd0156d715c40ac4b40536707387ce
Ayush <ayush.raj.sd@gmail.com>
2025-10-29 16:20:44 +0530

    kanban structure

--- LLD2/DOM/script3.js
+++ LLD2/DOM/script3.js
@@ -1,18 +1,26 @@
 const removeAllBtn = document.querySelector("#removeAll");
-const ul = document.querySelector("ul");
-const allDeleteButtons = document.querySelectorAll(".delete_button");
+// const ul = document.querySelector("ul");
+// const allDeleteButtons = document.querySelectorAll(".delete_button");
 
-for (let i = 0; i < allDeleteButtons.length; i++) {
-  allDeleteButtons[i].addEventListener("click", function (e) {
-    // go to the target element and then delete the parent
-    e.target.parentElement.remove();
-  });
-}
+// for (let i = 0; i < allDeleteButtons.length; i++) {
+//   allDeleteButtons[i].addEventListener("click", function (e) {
+//     // go to the target element and then delete the parent
+//     e.target.parentElement.remove();
+//   });
+// }
 
-removeAllBtn.addEventListener("click", function () {
-  //   const allListItems = ul.querySelectorAll("li");
-  //   for (let i = 0; i < allListItems.length; i++) {
-  //     ul.removeChild(allListItems[i]);
-  //   }
-  ul.innerHTML = "";
+// removeAllBtn.addEventListener("click", function () {
+//   //   const allListItems = ul.querySelectorAll("li");
+//   //   for (let i = 0; i < allListItems.length; i++) {
+//   //     ul.removeChild(allListItems[i]);
+//   //   }
+//   ul.innerHTML = "";
+// });
+
+const liElements = document.getElementsByTagName("li");
+
+removeAllBtn.addEventListener("click", function (e) {
+  for (let i = liElements.length - 1; i >= 0; i--) {
+    liElements[i].remove();
+  }
 });
