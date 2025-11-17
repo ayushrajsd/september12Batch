1f7542e83fe20a12922c6c712e25624dc2add1c7
Ayush <ayush.raj.sd@gmail.com>
2025-11-17 07:43:18 +0530

    q3

--- LLD2/async/practiseQuestions/questions.js
+++ LLD2/async/practiseQuestions/questions.js
@@ -20,3 +20,29 @@ function fakeDownload(url, callback) {
 fakeDownload("https://example.com/file", (message) => {
   console.log(message); // Expected Output: "Download complete!" (after 2 sec)
 });
+
+// q2. convert callback to a promise
+
+// Usage:
+fakeDownload("https://example.com/file").then((message) =>
+  console.log(message)
+); // Expected Output: "Download complete!" (after 2 sec)
+
+/***
+   * Q3
+   * Fetch a random cat fact from "https://catfact.ninja/fact" and log it.
+Use promise chaining to handle the fetch request. Log any errors that occur.
+
+Starter Code:
+
+// Task:
+// Fetch a random cat fact from "https://catfact.ninja/fact" and log it.
+// Use promise chaining to handle the fetch request. Log any errors that occur.
+
+function fetchCatFact() {
+  // Write code here...
+}
+
+// Usage:
+fetchCatFact();
+   */
